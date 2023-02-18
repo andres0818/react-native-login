32fe0b7443b24977616b632c327ae8d2cc52d495
andres0818 <c.andres.perez.h@gmail.com>
2023-02-18 01:26:39 -0500

    feat: my first app whit react native

--- App.js
+++ App.js
@@ -1,29 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { StyleSheet, TouchableOpacity, Platform, Text, View, TextInput, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import fondo from './native.webp'
+import ButtonGradient from './ButtonGradient';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Hello</Text>
-      <Text style={styles.titulo}>Sigin in to your account</Text>
-      <TextInput placeholder='Enter your email'/>
-      <TextInput placeholder='Password'/>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Image style={styles.contianerSvg} source={fondo} />
+        <Text style={styles.titulo}>Hello</Text>
+        <Text style={styles.subTitle}>Sigin in to your account</Text>
+        <TextInput style={styles.textInput} placeholder='Enter your email' />
+        <TextInput secureTextEntry style={styles.textInput} placeholder='Password' />
+        <Text style={styles.forgotPassword}>Forgot your password</Text>
+        <ButtonGradient />
+        <Text style={styles.forgotPassword}>Don't have an account</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    backgroundColor: '#f1f1f1',
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  container: {
+
     alignItems: 'center',
     justifyContent: 'center',
   },
+  contianerSvg: {
+    width: '100%',
+    height: "180%",
+    ...Platform.select({
+      android: {
+        transform: [{ translateY: -50 }],
+        height: "35%",
+
+      },
+      ios: {
+        transform: [{ translateY: -50 }],
+        height: "40%",
+
+      }
+    })
+
+  },
   titulo: {
-    fontSize: 30,
-    color:'black',
+    fontSize: 80,
+    color: '#34434D',
     fontWeight: 'bold',
-    
+  },
+  subTitle: {
+    fontSize: 30,
+    color: 'gray',
+  },
+  textInput: {
+    padding: 10,
+    width: '80%',
+    height: 50,
+    paddingStart: 30,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: 'white',
+  },
+  forgotPassword: {
+    color: 'gray',
+    fontSize: 20,
+    marginTop: 20,
   }
 });
---
+++ ButtonGradient.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+const ButtonGradient = () => {
+    return (
+        <TouchableOpacity style={styles.container} >
+            <LinearGradient
+                colors={['blue', '#ff3cbd',]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                style={styles.button}
+            >
+                <Text style={styles.text}>SIGN IN</Text>
+            </LinearGradient>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        width: 200,
+        marginTop:60,
+        
+    },
+    text: {
+        fontSize: 14,
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    button: {
+        width: '80%',
+        height: 50,
+        borderRadius: 25,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+
+    }
+})
+
+export default ButtonGradient
